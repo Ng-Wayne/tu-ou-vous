21d3a66ce3b253a7baf2e95b283188545f8b9143
Wayne Ng <ngzuwayne@gmail.com>
2026-06-19 15:19:31 +0800

    First commit

---
+++ src/main/java/com/wayneng/tuouvous/TuouvousApplication.java
@@ -0,0 +1,13 @@
+package com.wayneng.tuouvous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuouvousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuouvousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wayneng/tuouvous/TuouvousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayneng.tuouvous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuouvousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
