066510b939a5abb37d76b6149ed6bfbf0805cd13
Wayne Ng <ngzuwayne@gmail.com>
2026-06-19 17:14:32 +0800

    Created an Ollama service for interacting with Ollama

---
+++ src/main/java/com/wayneng/tuouvous/service/OllamaService.java
@@ -0,0 +1,11 @@
+package com.wayneng.tuouvous.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OllamaService {
+
+    public String translate(String prompt) {
+        return "Bonjour";
+    }
+}
