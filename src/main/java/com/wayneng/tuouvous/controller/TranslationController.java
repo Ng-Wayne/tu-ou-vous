4783ba68e2df6c4db594429f01c4af7031766712
Wayne Ng <ngzuwayne@gmail.com>
2026-06-19 16:54:50 +0800

    Created the translation service

--- src/main/java/com/wayneng/tuouvous/controller/TranslationController.java
+++ src/main/java/com/wayneng/tuouvous/controller/TranslationController.java
@@ -2,16 +2,26 @@ package com.wayneng.tuouvous.controller;
 
 import com.wayneng.tuouvous.dto.TranslationRequest;
 import com.wayneng.tuouvous.dto.TranslationResponse;
+import com.wayneng.tuouvous.service.TranslationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/translate")
+@RequiredArgsConstructor
 public class TranslationController {
+
+    private final TranslationService translationService;
 
     @PostMapping
     public TranslationResponse translate(
             @RequestBody TranslationRequest request) {
 
-        return new TranslationResponse("Bonjour");
+        return new TranslationResponse(
+                translationService.translate(
+                        request.text(),
+                        request.level()
+                )
+        );
     }
 }
---
+++ src/main/java/com/wayneng/tuouvous/service/TranslationService.java
@@ -0,0 +1,11 @@
+package com.wayneng.tuouvous.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TranslationService {
+
+    public String translate(String text, int level) {
+        return "Bonjour";
+    }
+}
